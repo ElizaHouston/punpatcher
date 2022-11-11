56447c734d03903ba7cffefa6a5a5c78b1debd85
ElizaHouston <108429810+ElizaHouston@users.noreply.github.com>
2022-11-11 12:54:29 -0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -46,7 +46,7 @@ const deprecateCategory = async (identifier: string) => {
 (async () => {
     const puns = await loadFromFile("./patches/create.json");
     for (const pun of puns) {
-        let response = await deprecateCategory(pun.short);
+        let response = await createCategory(pun);
         console.info(JSON.stringify(response, null, 2));
     }
 })();
